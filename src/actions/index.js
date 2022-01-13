e7add7380c7b05f8e4d7f1223c525dbb0b18f8e9
Iriko0606 <eriko.i0606@gmail.com>
2022-01-13 16:10:23 +0900

    feature: actionCreator

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export const INCREMET = 'INCREMENT'
+export const DECREMET = 'DECREMENT'
+
+export const increment = () => ({
+    type: 'INCREMENT'
+})
+
+export const decrement = () => ({
+    type : "DECREMENT"
+})
